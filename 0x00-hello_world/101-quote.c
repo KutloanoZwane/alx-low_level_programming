0eb71a25e82d224f272a3dd44fd3f37b6797d1aa
KutloanoZwane <kutloanozwane2012@gmail.com>
2023-07-13 15:12:38 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,18 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
+#!/bin/bash
+gcc -S -masm=intel $CFILE
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -10,5 +25,5 @@ int main(void)
 {
 	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	return (1);
 }
